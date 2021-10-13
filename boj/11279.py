d87ecede32c0b507b850fb158cf5b59e11cde804
Wooseong Yang/양우성 <dntjddid1@gmail.com>
2021-10-13 23:21:17 +0900

    boj: 11279

--- boj/11279.py
+++ boj/11279.py
@@ -3,14 +3,29 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-heap = []
+plus_heap = []
+minus_heap = []
 
 for _ in range(n):
     i = int(input())
+    # print(minus_heap, plus_heap)
     if i == 0:
-        if len(heap) == 0:
+        if len(plus_heap) == 0 and len(minus_heap) == 0:
             print(0)
         else:
-            print(-heapq.heappop(heap))
+            if not plus_heap and not minus_heap:
+                print(0)
+            elif not plus_heap:
+                print(-heapq.heappop(minus_heap))
+            elif not minus_heap:
+                print(heapq.heappop(plus_heap))
+            elif plus_heap[0] >= minus_heap[0] and minus_heap:
+                print(-heapq.heappop(minus_heap))
+            elif plus_heap[0] < minus_heap[0] and plus_heap:
+                print(heapq.heappop(plus_heap))
+
     else:
-        heapq.heappush(heap, -i)
+        if i > 0:
+            heapq.heappush(plus_heap, i)
+        elif i < 0:
+            heapq.heappush(minus_heap, -i)
