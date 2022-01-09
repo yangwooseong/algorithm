4674a2c500e5380feb1faf366af62e54bdae3cb7
Wooseong Yang/양우성 <dntjddid1@gmail.com>
2022-01-09 16:24:00 +0900

    programmers: 메뉴리뉴얼

--- programmers/src/메뉴리뉴얼.js
+++ programmers/src/메뉴리뉴얼.js
@@ -1,105 +1,51 @@
-// // function solution(orders, course) {
-// //   let answer = []
-// //   // 메뉴는 2개 이상, 2명 이상의 손님으로부터 주문된 단품메뉴
-// //   let prev_elements = []
+function solution(orders, course) {
+  let answer = []
+  const dict = {}
 
-// //   function combination(elements, k, results) {
-// //     if (prev_elements.length === k) {
-// //       let str_tmp = prev_elements.slice().sort().join('')
-// //       results[str_tmp] =
-// //         results[str_tmp] === undefined ? 1 : results[str_tmp] + 1
-// //       return
-// //     }
+  const addCombinationToDict = (current, idx, order) => {
+    if (course.includes(current.length)) {
+      const string = current.slice().sort().join('')
+      // 여기서 slice를 빼면 current 를 바꿔버리기 때문에 slice를 꼭 넣어야 한다.
+      dict[string] = dict[string] === undefined ? 1 : dict[string] + 1
 
-// //     for (let i = 0; i < elements.length; i++) {
-// //       let next_elements = elements.slice(i + 1, elements.length)
-// //       prev_elements.push(elements[i])
-// //       combination(next_elements, k, results)
-// //       prev_elements.pop()
-// //     }
-// //   }
+      if (current.length === course[course.length - 1]) return
+    }
 
-// //   let arr = []
-// //   orders.map((order) => {
-// //     arr.push(order.split(''))
-// //   })
+    for (let i = idx; i < order.length; i++) {
+      current.push(order[i])
+      addCombinationToDict(current, i + 1, order)
+      current.pop()
+    }
+  }
 
-// //   let results = []
+  for (const order of orders) {
+    addCombinationToDict([], 0, order)
+  }
 
-// //   for (let i in course) {
-// //     results.push({})
-// //     for (let j in arr) {
-// //       combination(arr[j], course[i], results[i])
-// //     }
-// //     console.log(results)
-// //   }
+  const arr = []
+  for (const [key, value] of Object.entries(dict)) {
+    if (value === 1) continue
+    arr.push([key, value])
+  }
 
-// //   results.map((result) => {
-// //     let max = 1
-// //     for (let i in result) {
-// //       if (max < result[i]) {
-// //         max = result[i]
-// //       }
-// //     }
+  arr.sort((a, b) =>
+    a[0].length !== b[0].length ? a[0].length - b[0].length : b[1] - a[1],
+  )
 
-// //     for (let j in result) {
-// //       if (result[j] === max && max > 1) {
-// //         answer.push(j)
-// //       }
-// //     }
-// //   })
+  for (const num of course) {
+    const candidiates = arr.filter((item) => item[0].length === num)
+    if (!candidiates.length) continue
+    const max = candidiates[0][1]
+    candidiates
+      .filter((item) => item[1] === max)
+      .map((item) => answer.push(item[0]))
+  }
 
-// //   return answer.sort()
-// // }
+  answer.sort()
+  console.log(answer)
+  return answer
+}
 
-// // const orders = ['ABCFG', 'AC', 'CDE', 'ACDE', 'BCFG', 'ACDEH']
-
-// // solution(orders, [2, 3, 4])
-
-// function solution(orders, course) {
-//   const dict = {}
-//   const combination = (string, idx, arr) => {
-//     if (course.includes(arr.length)) {
-//       const course = arr.sort().join('')
-//       dict[course] = dict[course] === undefined ? 1 : dict[course] + 1
-
-//       if (arr.length === course.slice(-1)) return
-//     }
-
-//     for (let i = idx; i < string.length; i++) {
-//       arr.push(string[i])
-//       combination(string, i + 1, arr)
-//       arr.pop()
-//     }
-//   }
-
-//   for (const order of orders) {
-//     combination(order, 0, [])
-//   }
-
-//   console.log(dict)
-//   const map = new Map()
-
-//   course.map((v) => map.set(v, []))
-
-//   for (const [key, value] of Object.entries(dict)) {
-//     if (value > 1) {
-//       map.get(key.length).push(key)
-//     }
-//   }
-
-//   const answer = []
-//   console.log(map)
-//   for (const value of map.values()) {
-//     const longest = value.sort((a, b) => b.length - a.length)[0].length
-//     console.log(longest)
-//     answer.concat(value.filter((v) => v.length === longest))
-//   }
-//   console.log(answer)
-
-//   console.log(answer)
-// }
-
-// const orders = ['ABCFG', 'AC', 'CDE', 'ACDE', 'BCFG', 'ACDEH']
-// const course = [2, 3, 4]
-// solution(orders, course)
+const orders = ['XYZ', 'XWY', 'WXA']
+const course = [2, 3, 4]
+solution(orders, course)
