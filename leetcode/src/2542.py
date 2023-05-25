220e3a916dd27037fb84eb210fef91d7a5c4a8cc
yangwooseong <andrew@channel.io>
2023-05-25 23:42:07 +0900

    leet(2542)

---
+++ leetcode/src/2542.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        total = res = 0
+        h = []
+
+        for a, b in sorted(list(zip(nums1, nums2)), key=lambda ab: -ab[1]):
+            total += a
+            heappush(h, a)
+
+            if len(h) > k:
+                total -= heappop(h)
+            if len(h) == k:
+                res = max(res, total * b)
+        return res
