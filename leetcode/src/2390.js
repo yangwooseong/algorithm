93cca5bfa8c72341b5d1710ea62a8e09c7220722
yangwooseong <andrew@channel.io>
2023-06-02 01:04:01 +0900

    leet(2390)

---
+++ leetcode/src/2390.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeStars = function(s) {
+  const stack = []
+  for (const char of s) {
+      if (char === '*') { stack.pop() }
+      else { stack.push(char) }
+  }
+
+  return stack.join('')
+};
